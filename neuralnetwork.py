ec1c4a971672da1a85dd8d43b19ee7d9f27053f6
Robbie <ittociwam@hotmail.com>
2016-02-14 12:06:44 -0800

    neural network is now multilayered and returns an array of predictions after running through the sigmoid function
    Signed-off-by: Robbie <ittociwam@hotmail.com>

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -90,64 +90,106 @@ class Dataset:
         self.std_test = (self.data_test - self.data_train.mean()) / self.data_train.std()
 
 class NeuralNetwork:
-    def __init__(self, learning_rate, data, targets, inputs, bias=-1):
+    def __init__(self, learning_rate, data, targets, inputs, num_layers=2):
         self.learning_rate = learning_rate
         self.data = data
-        self.bias = bias
+        self.bias = -1
         self.targets = targets
         self.labels = np.unique(targets)
         self.inputs = inputs
         self.neurons = []
-        num_inputs = len(self.inputs.columns)
+        self.num_inputs = len(self.data.columns)
         self.input_list = []
-        self.label_dict = defaultdict(list)
-        print("inputs: \n", self.inputs)
-        for label in self.labels:
-            new_neuron = self.Neuron(num_inputs, label)
-            self.neurons.append(new_neuron)
+        self.layers = []
+        #self.label_dict = defaultdict(list)
+        print("data_inputs: \n", self.data)
+        self.build_network(num_layers)
 
+    # build_network will build the network, the network will be stored in a multidimentional array
+    # A network with 3 input neurons, 1 layer of 2 hidden neurons and 4 output neurons would be stored like this:
+    # [[Neuron, Neuron, Neuron], [Neuron, Neuron], [Neuron, Neuron, Neuron, Neuron]]
+    # The initial layer will be built with the number of inputs of the dataset and the rest will be built with the
+    # number of neurons from the previous layer
+    def build_network(self, num_layers):
+        #The initial inputs for layer 1 will be the number of inputs of the data
+        layer_inputs = self.num_inputs
+        # if the user has specified more than 1 layer
+        if num_layers > 1:
+            #loop through the layers except the last layer which we will handle separately
+            for i in range(num_layers - 1):
+                this_layer = []
+                #prompt user for num neurons in this layer
+                num_neurons = 2
+                for j in range(num_neurons):
+                    #make that many neurons in the layer
+                    new_neuron = self.Neuron(layer_inputs)
+                    this_layer.append(new_neuron)
+                #grab the number of neurons to use as layer inputs for our next layer
+                layer_inputs = num_neurons
+                #append that layer to the NN layers array
+                self.layers.append(this_layer)
+        # this will either be the last layer of our MLP or a single layer
+        last_layer = []
+        for label in self.labels:
+            new_neuron = self.Neuron(layer_inputs)
+            last_layer.append(new_neuron)
+        self.layers.append(last_layer)
 
     def train_network(self):
-        outputs = []
+        network_output = []
+        counter = 0
         #loop through each row
-        for row in self.data.iterrows():
-            print("row calculating: ", row)
-            output = []
-            for neuron in self.neurons:
-                output.append(neuron.compute_activation(row))
-            outputs.append(output)
-        print(outputs)
-
+        for data_inputs in self.data.iterrows():
+            outputs = []
+            iteration_inputs = data_inputs
+            for layer in (self.layers):
+                outputs = []
+                for neuron in layer:
+                    counter += 1
+                    outputs.append(neuron.compute_activation(iteration_inputs))
+                iteration_inputs = (1, outputs)
+            #error = sum_of_squares_error(outputs) #will compare with targets
+            #go_back_and_change_weights(error)
+            network_output.append(outputs)
+        print(network_output, "COUNTER: ", counter)
 
 
     class Neuron:
-        def __init__(self, num_inputs, label):
+        def __init__(self, num_inputs):
             self.rangemax = 1.0
             self.rangemin = -1.0
             self.inputs = []
-            self.label = label
             self.threshold = 0
             for i in range(num_inputs):
-                self.inputs.append(self.Input(i,random.uniform(self.rangemin, self.rangemax) ))
+                print("appending input number: ", i)
+                self.inputs.append(self.Input(random.uniform(self.rangemin, self.rangemax)))
             #make bias input the last input
-            self.inputs.append(self.Input(num_inputs + 1, random.uniform(self.rangemin, self.rangemax)))
-            print("making a neuron")
+            self.inputs.append(self.Input(random.uniform(self.rangemin, self.rangemax)))
+            print("making a neuron with ", num_inputs, " inputs\n")
 
         def compute_activation(self, input):
+            #assert input is len(self.inputs)
             _sum = 0
             #loop through all inputs
-            for i in range(len(self.inputs) - 1):
+            #print("for compute we are looping through range: ", range(len(self.inputs) - 1))
+            for i in range(len(self.inputs) -1):
                 _sum += self.inputs[i].weight * input[1][i]
             #calculate for bias input -1 gets last element in an array
             _sum += self.inputs[-1].weight * 1
-            if _sum > self.threshold:
-                return 1
-            else:
-                return 0
+            self.sigmoid(_sum)
+            return _sum #> self.threshold:
+               # return 1
+           # else:
+              #  return 0
+
+        def sigmoid(self, x):
+            return 1 / (1 + np.exp(-1))
+
+        def dsigmoid(self, y):
+            return y ( (1.0 - y))
 
         class Input:
-            def __init__(self, attribute, weight):
-                self.attribute = attribute
+            def __init__(self, weight):
                 self.weight = weight
 
 
@@ -155,7 +197,7 @@ class NeuralNetwork:
 
 print("**********************CSV**************************")
 
-my_dataset = Dataset('iris.csv', .3, 3333, False)
+my_dataset = Dataset('indian', .3, 3333, False)
 
 nn = NeuralNetwork(.3, my_dataset.data_train, my_dataset.target_train, my_dataset.data_test)
 
